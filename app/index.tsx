f1e8b758602dfdcbbde04286530d9644fb83702f
jasim-binumar <jasimbinumar@gmail.com>
2024-11-02 13:37:07 +0500

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+
+import { View, Text, ScrollView } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <ScrollView>
+        <Text className="text-red-400 flex items-center justify-center">index11111</Text>
+        
+    </ScrollView>
+    
+  )
+}
+
+export default index
